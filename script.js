21f15db57ff5b1cc4eedcc909682d0a9e952b990
jconnor27 <johndconnor97@gmail.com>
2023-04-25 23:18:00 -0400

    Load Data now occurs on home page

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+async function mainEvent() {
+    const homePageLoadBox = document.querySelector("#home_page_load_box");
+    const loadDataButton = document.querySelector("#load_data_button");
+    const homePageDoneLoadingBox = document.querySelector("#home_page_done_loading_box");
+    const statusBoxNotLoaded = document.querySelector("#status_box_not_loaded");
+    const statusBoxLoaded = document.querySelector("#status_box_loaded");
+    const finalPageLink = document.querySelector("#final_page_link");
+
+
+    loadDataButton.addEventListener("click", async (submitEvent) => {
+        console.log("Fired - loadDataButton");
+
+        console.log("Fetching - loading professor - a list of all professors");
+        const professorsListRaw = await fetch("https://api.umd.io/v1/professors");
+        const professorsListGeneral = await professorsListRaw.json();
+
+        localStorage.setItem('professorsListGeneral', JSON.stringify(professorsListGeneral));
+
+        homePageLoadBox.classList.add("hidden");
+        homePageDoneLoadingBox.classList.remove("hidden");
+        
+        statusBoxNotLoaded.classList.add("hidden");
+        statusBoxLoaded.classList.remove("hidden");
+
+        finalPageLink.classList.remove("hidden");
+    })
+
+}
+
+document.addEventListener("DOMContentLoaded", async () => mainEvent()); // the async keyword means we can make API requests
